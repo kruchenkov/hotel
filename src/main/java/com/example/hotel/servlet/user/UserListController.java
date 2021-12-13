fe22d9896c9662696accc867bb12ecd623a108e0
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 02:53:34 +0300

    Realized method doGet with userService method finAll

--- src/main/java/com/example/hotel/servlet/user/UserListController.java
+++ src/main/java/com/example/hotel/servlet/user/UserListController.java
@@ -1,9 +1,15 @@
 package com.example.hotel.servlet.user;
 
+import com.example.hotel.entity.User;
+import com.example.hotel.service.ServiceException;
 import com.example.hotel.service.UserService;
-import com.example.hotel.service.impl.UserServiceImpl;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 public class UserListController extends HttpServlet {
 
@@ -11,5 +17,16 @@ public class UserListController extends HttpServlet {
 
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<User> users = userService.findAll();
+            req.setAttribute("users", users);
+            req.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(req, resp);
+        } catch (ServiceException e) {
+            throw new ServletException(e);
+        }
     }
 }
