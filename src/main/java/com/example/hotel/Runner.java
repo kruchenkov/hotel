ec620e1e492f3619ce7f418659bf68ccd57be258
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 02:08:36 +0300

    Added another test realization of main method but with userService methods

--- src/main/java/com/example/hotel/Runner.java
+++ src/main/java/com/example/hotel/Runner.java
@@ -4,24 +4,29 @@ import com.example.hotel.dao.DaoException;
 import com.example.hotel.dao.UserDao;
 import com.example.hotel.dao.impl.UserDaoImpl;
 import com.example.hotel.entity.User;
+import com.example.hotel.service.ServiceException;
+import com.example.hotel.service.UserService;
+import com.example.hotel.service.UserServiceImpl;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
 public class Runner {
-    public static void main(String[] args) throws DaoException {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("/jdbc-context-xml.xml");
+    public static void main(String[] args) throws DaoException, ServiceException {
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("/jdbc-context-xml.xml", "/service-context-xml.xml");
         UserDao userDao = ctx.getBean("userDao", UserDaoImpl.class);
+        UserService userService = ctx.getBean("userService", UserServiceImpl.class);
 
         User user;
 
-        List<User> users = userDao.readAll();
+        List<User> users = userService.findAll();
         for (User user1 : users) {
             System.out.println(user1);
         }
 
-        user = userDao.findByLoginAndPassword("John", "1234");
+        user = userService.login("John", "1234");
         System.out.println(user);
     }
 }
