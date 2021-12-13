6bb7fa2119bea8968b8c9e6790e409b7a99c582d
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-13 23:25:10 +0300

    Implemented CRUD methods
    implemented methods:
    1. create(T entity)
    2. read(Long id)
    3. update(T entity)
    4. delete (Long id)

--- src/main/java/com/example/hotel/dao/Dao.java
+++ src/main/java/com/example/hotel/dao/Dao.java
@@ -3,4 +3,11 @@ package com.example.hotel.dao;
 import com.example.hotel.entity.Entity;
 
 public interface Dao<T extends Entity> {
+    Long create (T entity) throws DaoException;
+
+    T read(Long id) throws DaoException;
+
+    void update (T entity) throws DaoException;
+
+    void delete (Long id) throws DaoException;
 }
