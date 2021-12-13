e8bdd422cb39dd26b115775a3c0d0a83b2b3b903
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 01:04:37 +0300

    Realized read method with jdbcTemplate.queryForObject()

--- src/main/java/com/example/hotel/dao/impl/UserDaoImpl.java
+++ src/main/java/com/example/hotel/dao/impl/UserDaoImpl.java
@@ -2,6 +2,7 @@ package com.example.hotel.dao.impl;
 
 import com.example.hotel.dao.DaoException;
 import com.example.hotel.dao.UserDao;
+import com.example.hotel.dao.mapper.UserMapper;
 import com.example.hotel.entity.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,7 +30,9 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User read(Long id) throws DaoException {
-        return null;
+        String sql = "SELECT * FROM `user` WHERE id = ?";
+        User user = (User) jdbcTemplate.queryForObject(sql, new Object[]{id}, new UserMapper());
+        return user;
     }
 
     @Override
