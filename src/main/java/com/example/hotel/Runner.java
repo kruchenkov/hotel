9edd32f298e029dd9069eb0ef24d28a5241b4450
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 01:33:57 +0300

    Made Runner.java for implement main class for test realization

--- src/main/java/com/example/hotel/Runner.java
+++ src/main/java/com/example/hotel/Runner.java
@@ -1,4 +1,27 @@
 package com.example.hotel;
 
+import com.example.hotel.dao.DaoException;
+import com.example.hotel.dao.UserDao;
+import com.example.hotel.dao.impl.UserDaoImpl;
+import com.example.hotel.entity.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
 public class Runner {
+    public static void main(String[] args) throws DaoException {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("/jdbc-context-xml.xml");
+        UserDao userDao = ctx.getBean("userDao", UserDaoImpl.class);
+
+        User user;
+
+        List<User> users = userDao.readAll();
+        for (User user1 : users) {
+            System.out.println(user1);
+        }
+
+        user = userDao.findByLoginAndPassword("John", "1234");
+        System.out.println(user);
+    }
 }
