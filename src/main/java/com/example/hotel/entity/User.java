da3f9da6b0edf20f08ed6e43cc7a67210d4bef68
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-13 22:04:58 +0300

    Added user description fields
    description fields - login, password, role

--- src/main/java/com/example/hotel/entity/User.java
+++ src/main/java/com/example/hotel/entity/User.java
@@ -1,4 +1,7 @@
 package com.example.hotel.entity;
 
-public class User extends Entity{
+public class User extends Entity {
+    private String login;
+    private String password;
+    private Role role;
 }
