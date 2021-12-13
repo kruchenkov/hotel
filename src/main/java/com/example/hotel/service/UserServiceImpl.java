8cdaa31bf0f5199fb05cf8f76526c7bb15cb380e
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 01:53:57 +0300

    Implemented UserService.java in UserServiceImpl.java with default methods realization
    methods:
    1. findAll
    2. findById
    3. saveUser
    4. login
    5. delete

--- src/main/java/com/example/hotel/service/UserServiceImpl.java
+++ src/main/java/com/example/hotel/service/UserServiceImpl.java
@@ -1,4 +1,32 @@
 package com.example.hotel.service;
 
-public class UserServiceImpl {
+import com.example.hotel.entity.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public List<User> findAll() throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public void saveUser(User user) throws ServiceException {
+
+    }
+
+    @Override
+    public User login(String login, String password) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public void delete(List<Long> ids) throws ServiceException {
+
+    }
 }
