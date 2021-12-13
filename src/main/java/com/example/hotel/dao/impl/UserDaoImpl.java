1dc1389635ddd69ae5367ce30a5a23c8acc7d5ab
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 00:32:51 +0300

    Implemented UserDao interface with methods(default realization)
    methods: create, read, update, delete, readAll, findByLoginAndPassword

--- src/main/java/com/example/hotel/dao/impl/UserDaoImpl.java
+++ src/main/java/com/example/hotel/dao/impl/UserDaoImpl.java
@@ -1,4 +1,39 @@
 package com.example.hotel.dao.impl;
 
-public class UserDaoImpl {
+import com.example.hotel.dao.DaoException;
+import com.example.hotel.dao.UserDao;
+import com.example.hotel.entity.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public Long create(User entity) throws DaoException {
+        return null;
+    }
+
+    @Override
+    public User read(Long id) throws DaoException {
+        return null;
+    }
+
+    @Override
+    public void update(User entity) throws DaoException {
+
+    }
+
+    @Override
+    public void delete(Long id) throws DaoException {
+
+    }
+
+    @Override
+    public List<User> readAll() throws DaoException {
+        return null;
+    }
+
+    @Override
+    public User findByLoginAndPassword(String login, String password) throws DaoException {
+        return null;
+    }
 }
