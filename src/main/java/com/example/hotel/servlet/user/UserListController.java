d7767207cf35a2c8e2b9a11bb20a1e90a2df7bf3
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 02:47:18 +0300

    Added row userService and inject bean userService in UserListController.java by setter

--- src/main/java/com/example/hotel/servlet/user/UserListController.java
+++ src/main/java/com/example/hotel/servlet/user/UserListController.java
@@ -1,6 +1,15 @@
 package com.example.hotel.servlet.user;
 
+import com.example.hotel.service.UserService;
+import com.example.hotel.service.impl.UserServiceImpl;
+
 import javax.servlet.http.HttpServlet;
 
 public class UserListController extends HttpServlet {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
