21b73a02f54637235a8edd3a45e134b2b0a8f04d
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 01:39:53 +0300

    Made 5 methods in UserService.java
    1. findAll
    2. findById
    3. SaveUser
    4. login
    5. delete

--- src/main/java/com/example/hotel/service/UserService.java
+++ src/main/java/com/example/hotel/service/UserService.java
@@ -1,4 +1,17 @@
 package com.example.hotel.service;
 
+import com.example.hotel.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> findAll() throws ServiceException;
+
+    User findById(Long id) throws ServiceException;
+
+    void saveUser(User user) throws ServiceException;
+
+    User login(String login, String password) throws ServiceException;
+
+    void delete(List<Long> ids) throws ServiceException;
 }
