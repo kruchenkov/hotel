dc83191a654e595dcc7c2f8e64fa35993684100b
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 00:22:19 +0300

    Implemented interface RowMapper and method mapRow
    implemented interface to display ResultSet rows

--- src/main/java/com/example/hotel/dao/mapper/UserMapper.java
+++ src/main/java/com/example/hotel/dao/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package com.example.hotel.dao.mapper;
 
-public class UserMapper {
+import com.example.hotel.entity.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet resultSet, int i){
+        return null;
+    }
 }
