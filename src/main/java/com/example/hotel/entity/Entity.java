b07485cd3bf123bbff8af5787c018656b313e4c6
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-13 21:49:49 +0300

    Made getter and setter of id field in Entity.java

--- src/main/java/com/example/hotel/entity/Entity.java
+++ src/main/java/com/example/hotel/entity/Entity.java
@@ -1,5 +1,14 @@
 package com.example.hotel.entity;
 
 public abstract class Entity {
+
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
