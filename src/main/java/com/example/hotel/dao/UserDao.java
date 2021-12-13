a21de6ddd578f82a0d56afc5f17dd03b040abe8c
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-13 23:51:58 +0300

    Made to methods in UserDao.java
    1.readAll()
    2.findByLoginAndPassword(String login, password)

--- src/main/java/com/example/hotel/dao/UserDao.java
+++ src/main/java/com/example/hotel/dao/UserDao.java
@@ -2,5 +2,10 @@ package com.example.hotel.dao;
 
 import com.example.hotel.entity.User;
 
+import java.util.List;
+
 public interface UserDao extends Dao<User>{
+    List<User> readAll() throws DaoException;
+
+    User findByLoginAndPassword(String login, String password) throws DaoException;
 }
