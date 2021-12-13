b3e48aa8a1192db166899db1e298c9d371c54aef
Ilya Kruchenkov <kruchenkowunlimited@gmail.com>
2021-12-14 00:28:05 +0300

    Realized mapRow with User.java rows

--- src/main/java/com/example/hotel/dao/mapper/UserMapper.java
+++ src/main/java/com/example/hotel/dao/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.example.hotel.dao.mapper;
 
+import com.example.hotel.entity.Role;
 import com.example.hotel.entity.User;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -8,7 +9,12 @@ import java.sql.SQLException;
 
 public class UserMapper implements RowMapper<User> {
     @Override
-    public User mapRow(ResultSet resultSet, int i){
-        return null;
+    public User mapRow(ResultSet resultSet, int i) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setLogin(resultSet.getString("login"));
+        user.setPassword(resultSet.getString("password"));
+        user.setRole(Role.values()[resultSet.getInt("role")]);
+        return user;
     }
 }
